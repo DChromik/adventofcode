14c9e874eff71ee9b8ee8eb41e8fd59c6dd7bc14
Daniel Chromik <daniel.chromik@seznam.cz>
2022-12-03 21:20:40 +0100

    refactored to more functional style

--- 2022/src/days/day1/part1.rs
+++ 2022/src/days/day1/part1.rs
@@ -1,26 +1,12 @@
-pub fn compute_part_1(file_path: &str) -> Vec<i32> {
-    let small_input = std::fs::read_to_string(file_path)
-        .expect("Should have been able to read a file.");
-    let string_array = small_input.split("\n");
-    let mut highest_calories = vec![0];
-    let mut current_elf_calories = 0;
-    for substr in string_array {
-        let trimmed = substr.trim();
-        if trimmed.len() == 0 {
-            let mut insert_index = 0;
-            for (index, calories) in highest_calories.iter().enumerate() {
-                if calories < &current_elf_calories {
-                    insert_index = index;
-                    break;
-                }
-            }
-            highest_calories.insert(insert_index, current_elf_calories);
-            current_elf_calories = 0;
-        } else {
-            let calories = trimmed.parse::<i32>()
-                .expect("Should have parsed calories string");
-            current_elf_calories += calories;
-        }
-    }
-    return highest_calories;
+pub fn compute_part_1(file_path: &str) -> Vec<u32> {
+    let small_input = std::fs::read_to_string(file_path).unwrap();
+    let mut string_array = small_input
+        .lines()
+        .map(|l| l.parse::<u32>().ok())
+        .collect::<Vec<_>>()
+        .split(|line| line.is_none())
+        .map(|group| group.iter().map(|v| v.unwrap()).sum::<u32>())
+        .collect::<Vec<_>>();
+    string_array.sort_unstable_by(|a, b| b.cmp(a));
+    return string_array;
 }
