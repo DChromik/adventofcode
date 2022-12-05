9b5a5de8aaabdc2ea2fbf54b0eda2597c7a9efe9
Daniel Chromik <daniel.chromik@seznam.cz>
2022-12-05 19:26:51 +0100

    day3 part 2 solution submitted

--- 2022/src/days/day3/solution.rs
+++ 2022/src/days/day3/solution.rs
@@ -4,30 +4,61 @@ pub fn resolve_part_1(path: &Path) -> u64 {
     let file = read_to_string(path).unwrap();
     let summed_priorities = file.lines()
         .map(split_in_half)
-        .map(find_common_item)
-        .map(to_priority)
+        .map(find_common_item_in_pack)
+        .map(item_to_priority)
         .sum::<u64>();
     return summed_priorities;
 }
 
-pub fn resolve_part_2(_path: &Path) -> u64 {
-    return 0;
+pub fn resolve_part_2(path: &Path) -> u64 {
+    let file = read_to_string(path).unwrap();
+    let summed_priorities = to_grouped_lines(&file)
+        .iter()
+        .map(find_common_item_in_group)
+        .map(item_to_priority)
+        .sum::<u64>();
+    return summed_priorities;
 }
 
 fn split_in_half(line: &str) -> (&str, &str) {
     return line.split_at(line.len() / 2)
 }
 
-fn find_common_item(pack: (&str, &str)) -> u8 {
+fn find_common_item_in_pack(pack: (&str, &str)) -> u8 {
     let index = pack.0
         .find(|character| pack.1.find(character).is_some())
         .unwrap();
     return pack.0.as_bytes()[index];
 }
 
-fn to_priority(item: u8) -> u64 {
+fn find_common_item_in_group(group: &(&str, &str, &str)) -> u8 {
+    let index = group.0
+        .find(|character| {
+            group.1.find(character).is_some()
+            && group.2.find(character).is_some()
+        })
+        .unwrap();
+    return group.0.as_bytes()[index];
+}
+
+fn item_to_priority(item: u8) -> u64 {
     if item >= b'a' && item <= b'z' {
         return (item - b'a' + 1) as u64;
     }
     return (item - b'A' + 27) as u64;
+}
+
+fn to_grouped_lines(string: &String) -> Vec<(&str, &str, &str)> {
+    let lines = string.lines();
+    let mut i = 0;
+    let mut groups = Vec::new();
+    let mut group = [""; 3];
+    for line in lines {
+        group[i % group.len()] = line;
+        if i % 3 == 2 {
+            groups.push((group[0], group[1], group[2]));
+        }
+        i += 1;
+    }
+    return groups;
 }
