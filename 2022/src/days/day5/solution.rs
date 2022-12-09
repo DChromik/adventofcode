219530d33e16efbcf95462717c9d6d18e94299ab
Daniel Chromik <daniel.chromik@seznam.cz>
2022-12-09 22:06:21 +0100

    Day5 stack initialization added

--- 2022/src/days/day5/solution.rs
+++ 2022/src/days/day5/solution.rs
@@ -1,9 +1,38 @@
-use std::path::Path;
+use std::{path::Path, collections::VecDeque};
 
-pub fn resolve_part_1(_path: &Path) -> u64 {
-    return 0;
+pub fn resolve_part_1(string: &str) -> &str {
+    let stacks = initialize_stacks(string);
+    for stack in stacks {
+        for item in stack.iter() {
+            println!("{}", *item as char);
+        }
+        println!("");
+    }
+    return "";
 }
 
 pub fn resolve_part_2(_path: &Path) -> u64 {
     return 0;
+}
+
+fn initialize_stacks(string: &str) -> Vec<VecDeque<u8>> {
+    let stack_count = get_stack_count(string);
+    let mut stacks = vec![VecDeque::new(); stack_count];
+    let stack_lines = string.lines()
+        .filter(|line| line.contains('['))
+        .rev();
+    for line in stack_lines {
+        for (index, line_index) in (1..line.len()).step_by(4).enumerate() {
+            let item = line.as_bytes()[line_index];
+            if item != b' ' {
+                stacks[index].push_back(line.as_bytes()[line_index])
+            }
+        }
+    }
+    return stacks;
+}
+
+fn get_stack_count(string: &str) -> usize {
+    let line_length = string.lines().nth(0).unwrap().len();
+    return (line_length + 1) / 4;
 }
--- 2022/src/days/day5/tests.rs
+++ 2022/src/days/day5/tests.rs
@@ -1,21 +1,21 @@
 #[cfg(test)]
 mod tests {
-    use std::path::Path;
+    use std::{path::Path, fs::read_to_string};
 
-    use crate::days::day4;
-
+    use crate::days::day5;
 
     #[test]
     fn should_compute_part_1() {
-        let path = Path::new("./src/days/day4/input_small.txt");
-        let result = day4::resolve_part_1(path);
-        assert_eq!(result, 2);
+        let path = Path::new("./src/days/day5/input_small.txt");
+        let string = read_to_string(path).unwrap();
+        let result = day5::resolve_part_1(&string);
+        assert_eq!(result, "CMZ");
     }
 
-    #[test]
-    fn should_compute_part_2() {
-        let path = Path::new("./src/days/day3/input_small.txt");
-        let result = day4::resolve_part_2(path);
-        assert_eq!(result, 70);
-    }
+    // #[test]
+    // fn should_compute_part_2() {
+    //     let path = Path::new("./src/days/day3/input_small.txt");
+    //     let result = day4::resolve_part_2(path);
+    //     assert_eq!(result, 70);
+    // }
 }
