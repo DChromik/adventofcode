5cc41c21d47ba8ac28f3b485087609edcfa35a46
Daniel Chromik <daniel.chromik@seznam.cz>
2022-12-21 19:55:51 +0100

    Refactored solution

--- 2022/src/days/day7/solution.rs
+++ 2022/src/days/day7/solution.rs
@@ -2,11 +2,12 @@ pub fn resolve_part_1(string: &str) -> String {
     let mut directories = Vec::new();
     let mut summed_sizes = 0;
     for line in string.lines() {
-        let split_line = line.split(' ').collect::<Vec<_>>();
-        if is_command(split_line[0]) {
-            if split_line[1] == "cd" && split_line[2] != ".." {
+        let (line_type, value) = parse_line(line);
+        match line_type {
+            Type::EnterDirectory => {
                 directories.push(Directory::new());
-            } else if split_line[1] == "cd" {
+            }
+            Type::LeaveDirectory => {
                 let left_directory = directories.pop().unwrap();
                 if left_directory.size <= 100_000 {
                     summed_sizes += left_directory.size;
@@ -14,13 +15,11 @@ pub fn resolve_part_1(string: &str) -> String {
                 let current_directory = directories.last_mut().unwrap();
                 current_directory.add_size(left_directory.size);
             }
-        } else {
-            if split_line[0] == "dir" {
-                continue;
+            Type::File => {
+                let current_directory = directories.last_mut().unwrap();
+                current_directory.add_size(value.parse().unwrap());
             }
-            let current_directory = directories.last_mut().unwrap();
-            let size = split_line[0].parse::<usize>().unwrap();
-            current_directory.add_size(size);
+            _ => {}
         }
     }
     for _ in 0..directories.len() {
@@ -41,25 +40,22 @@ pub fn resolve_part_2(string: &str) -> String {
     let mut directories = Vec::new();
     let mut directory_sizes = Vec::new();
     for line in string.lines() {
-        let split_line = line.split(' ').collect::<Vec<_>>();
-        if is_command(split_line[0]) {
-            if split_line[1] == "cd" && split_line[2] != ".." {
+        let (line_type, value) = parse_line(line);
+        match line_type {
+            Type::EnterDirectory => {
                 directories.push(Directory::new());
-            } else if split_line[1] == "cd" {
+            }
+            Type::LeaveDirectory => {
                 let left_directory = directories.pop().unwrap();
-                if left_directory.size <= 100_000 {
-                    directory_sizes.push(left_directory.size);
-                }
+                directory_sizes.push(left_directory.size);
                 let current_directory = directories.last_mut().unwrap();
                 current_directory.add_size(left_directory.size);
             }
-        } else {
-            if split_line[0] == "dir" {
-                continue;
+            Type::File => {
+                let current_directory = directories.last_mut().unwrap();
+                current_directory.add_size(value.parse().unwrap());
             }
-            let current_directory = directories.last_mut().unwrap();
-            let size = split_line[0].parse::<usize>().unwrap();
-            current_directory.add_size(size);
+            _ => {}
         }
     }
     for _ in  0..directories.len() {
@@ -72,15 +68,40 @@ pub fn resolve_part_2(string: &str) -> String {
     let free_space = TOTAL_SPACE - directory_sizes.last().unwrap();
     let mut target_directory_size = REQUIRED_SPACE;
     for size in directory_sizes {
-        if size + free_space >= REQUIRED_SPACE && size < target_directory_size {
+        if (size + free_space) >= REQUIRED_SPACE && size < target_directory_size {
             target_directory_size = size;
         }
     }
     return target_directory_size.to_string();
 }
 
+enum Type {
+    LeaveDirectory,
+    EnterDirectory,
+    List,
+    Dir,
+    File,
+}
+
 fn is_command(line: &str) -> bool {
     line.as_bytes()[0] == b'$'
+}
+
+type ParsedLine = (Type, String);
+fn parse_line(line: &str) -> ParsedLine {
+    let split_line = line.split(' ').collect::<Vec<_>>();
+    if split_line[1] == "cd" {
+        if split_line[2] == ".." {
+            return (Type::LeaveDirectory, "..".to_string());
+        } else {
+            return (Type::EnterDirectory, split_line[2].to_string());
+        }
+    } else if split_line[0] == "dir" {
+        return (Type::Dir, split_line[1].to_string());
+    } else if split_line[1] == "ls" {
+        return (Type::List, "".to_string());
+    }
+    return (Type::File, split_line[0].to_string());
 }
 
 struct Directory {
