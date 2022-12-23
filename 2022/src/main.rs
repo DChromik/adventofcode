d92792043f9d74c066a59da1ab69095f8d43dcef
Daniel Chromik <daniel.chromik@seznam.cz>
2022-12-23 19:26:21 +0100

    Real input files removed

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -5,32 +5,33 @@ mod days;
 mod utils;
 
 fn main() {
-    println!("Day 1");
-    let result = day1::compute_part_1("src/days/day1/input.txt");
-    let top_calories = result[0] + result[1] + result[2];
-    println!("  - part 2: {}", top_calories);
-
-    println!("Day 2");
-    let result = day2::resolve_part_1("src/days/day2/input.txt");
-    println!("  - part 1: {}", result);
-    let result = day2::resolve_part_2("src/days/day2/input.txt");
-    println!("  - part 2: {}", result);
-
-    println!("Day 3");
-    let result = day3::resolve_part_1(Path::new("src/days/day3/input.txt"));
-    println!("  - part 1: {}", result);
-    let result = day3::resolve_part_2(Path::new("src/days/day3/input.txt"));
-    println!("  - part 2: {}", result);
-
-    println!("Day 4");
-    let result = day4::resolve_part_1(Path::new("src/days/day4/input.txt"));
-    println!("  - part 1: {}", result);
-    let result = day4::resolve_part_2(Path::new("src/days/day4/input.txt"));
-    println!("  - part 2: {}", result);
+    // println!("Day 1");
+    // let result = day1::compute_part_1("src/days/day1/input.txt");
+    // let top_calories = result[0] + result[1] + result[2];
+    // println!("  - part 2: {}", top_calories);
+    //
+    // println!("Day 2");
+    // let result = day2::resolve_part_1("src/days/day2/input.txt");
+    // println!("  - part 1: {}", result);
+    // let result = day2::resolve_part_2("src/days/day2/input.txt");
+    // println!("  - part 2: {}", result);
+    //
+    // println!("Day 3");
+    // let result = day3::resolve_part_1(Path::new("src/days/day3/input.txt"));
+    // println!("  - part 1: {}", result);
+    // let result = day3::resolve_part_2(Path::new("src/days/day3/input.txt"));
+    // println!("  - part 2: {}", result);
+    //
+    // println!("Day 4");
+    // let result = day4::resolve_part_1(Path::new("src/days/day4/input.txt"));
+    // println!("  - part 1: {}", result);
+    // let result = day4::resolve_part_2(Path::new("src/days/day4/input.txt"));
+    // println!("  - part 2: {}", result);
 
     resolve_day(5);
     resolve_day(6);
     resolve_day(7);
+    resolve_day(8);
 }
 
 fn resolve_day(day: usize) {
--- 2022/src/utils/file.rs
+++ 2022/src/utils/file.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 
 pub fn read_input_file(day: usize) -> String {
-    let path = format!("src/days/day{}/input.txt", day);
+    let path = format!("./inputs/day{}.txt", day);
     return read_to_string(path).unwrap();
 }
 
