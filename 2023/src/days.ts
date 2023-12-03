ee4f3d44992cfe7a81eed29ef9b05a89c8568673
Daniel Chromik <daniel.chromik@seznam.cz>
2023-12-03 11:31:40 +0100

    Command line argument formatting added

---
+++ 2023/src/days.ts
@@ -0,0 +1,5 @@
+import { resolvePart1, resolvePart2 } from './day1';
+
+export const resolve = [
+	[resolvePart1, resolvePart2],
+];
--- 2023/src/index.ts
+++ 2023/src/index.ts
@@ -1,16 +1,18 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import * as path from 'path';
 import { createReadStream } from 'fs';
-import { resolvePart1, resolvePart2
- } from './day1/solution';
+import { resolve } from './days';
 
-async function main() {
-	let file = createReadStream(path.resolve(__dirname, '../inputs/day1.txt'));
-	const part1 = await resolvePart1(file);
-	console.log('Part 1: ', part1);
+const args = yargs(hideBin(process.argv)).options({
+	d: { alias: 'day', describe: 'AoC 2023 day', type: 'number', demandOption: true },
+	p: { alias: 'part', describe: 'AoC part 1 or 2 of the days problem', type: 'number', demandOption: true, choices: [1, 2] }
+}).parseSync();
 
-	file = createReadStream(path.resolve(__dirname, '../inputs/day1.txt'));
-	const part2 = await resolvePart2(file);
-	console.log('Part 2: ', part2);
-}
+(async function main() {
+	const { d: day, p: part } = args;
+	const file = createReadStream(path.resolve(__dirname, `../inputs/day${day}.txt`));
+	const result = await resolve[day - 1][part - 1](file);
+	console.log('Results for day %d, part %d: %s', day, part, result);
+})();
 
-main();
