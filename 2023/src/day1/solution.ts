bad7a676dc40db306071ded57354a78fbc4aa242
Daniel Chromik <daniel.chromik@seznam.cz>
2023-12-03 10:31:53 +0100

    Day 1 part 2 resolved

--- 2023/src/day1/solution.ts
+++ 2023/src/day1/solution.ts
@@ -8,7 +8,9 @@ export async function resolvePart1(stream: ReadStream): Promise<number> {
 }
 
 export async function resolvePart2(stream: ReadStream): Promise<number> {
-	return 0;
+	return reduceLines(stream, (sum, line) => {
+		return sum + handleLine2(line);
+	}, 0);
 }
 
 async function reduceLines<T>(stream: ReadStream, handleLine: (accumulator: T, line: string) => T, initialValue: T): Promise<T> {
@@ -34,4 +36,47 @@ function handleLine(line: string): number {
 		return acc;
 	}, []);
 	return numbers[0] * 10 + numbers[1];
+}
+
+function handleLine2(line: string): number {
+	const numbers = line.split('').reduce((acc: number[], value, index) => {
+		let number = Number.parseInt(value);
+		if (isNaN(number)) {
+			const words = extractWords(line, index);
+			number = wordToNumber(words);
+}
+		if (!isNaN(number)) {
+			if (acc.length === 0) {
+				return [number, number];
+			}
+			return [acc[0], number];
+		}
+		return acc;
+	}, []);
+	return numbers[0] * 10 + numbers[1];
+}
+
+const numberWords = [
+	'one',
+	'two',
+	'three',
+	'four',
+	'five',
+	'six',
+	'seven',
+	'eight',
+	'nine'
+];
+
+function extractWords(line: string, startingIndex: number): string[] {
+	return [
+		line.substring(startingIndex, startingIndex + 3),
+		line.substring(startingIndex, startingIndex + 4),
+		line.substring(startingIndex, startingIndex + 5),
+	]
+}
+
+function wordToNumber(words: string[]): number {
+	const index = numberWords.findIndex((numberWord) => words.includes(numberWord));
+	return index + 1 || NaN;
 }
--- 2023/src/index.ts
+++ 2023/src/index.ts
@@ -1,10 +1,16 @@
 import * as path from 'path';
 import { createReadStream } from 'fs';
-import { resolvePart1 } from './day1/solution';
+import { resolvePart1, resolvePart2
+ } from './day1/solution';
 
 async function main() {
-	const file = createReadStream(path.resolve(__dirname, '../inputs/day1.txt'));
-	console.log(await resolvePart1(file));
+	let file = createReadStream(path.resolve(__dirname, '../inputs/day1.txt'));
+	const part1 = await resolvePart1(file);
+	console.log('Part 1: ', part1);
+
+	file = createReadStream(path.resolve(__dirname, '../inputs/day1.txt'));
+	const part2 = await resolvePart2(file);
+	console.log('Part 2: ', part2);
 }
 
 main();
