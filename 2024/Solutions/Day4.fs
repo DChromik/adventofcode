c8889d2fa67cd4f82102440b721b1ddd0f01cb97
Daniel Chromik <daniel.chromik@dawiso.com>
2024-12-04 15:14:40 +0100

    Refactor

--- 2024/Solutions/Day4.fs
+++ 2024/Solutions/Day4.fs
@@ -3,14 +3,10 @@ module Day4
 let direction =
     [| (1, 0); (-1, 0); (0, 1); (0, -1); (1, 1); (1, -1); (-1, 1); (-1, -1) |]
 
-let targetWord = "XMAS"
+let linesToGrid (lines: seq<string>) : char[][] =
+    lines |> Seq.map (fun line -> line.TrimEnd().ToCharArray()) |> Seq.toArray
 
-let linesToArray (lines: seq<string>) : char[][] =
-    lines
-    |> Seq.map (fun line -> line.Replace("\r", "") |> Seq.toArray)
-    |> Seq.toArray
-
-let getWord (grid: char[][]) (length: int) (x: int, y: int) (dx: int, dy: int) : seq<char> =
+let getWord (grid: char[][]) (length: int) (x: int, y: int) (dx: int, dy: int) : string =
     seq {
         let endX = x + (dx * (length - 1))
         let endY = y + (dy * (length - 1))
@@ -22,14 +18,16 @@ let getWord (grid: char[][]) (length: int) (x: int, y: int) (dx: int, dy: int) :
             for i = 0 to length - 1 do
                 yield grid.[y + dy * i].[x + dx * i]
     }
+    |> Seq.toArray
+    |> System.String
 
-let hasDiagonalMas (grid: char[][]) (x: int, y: int) : int =
+let hasDiagonalMas (grid: char[][]) (x: int) (y: int) : int =
     let isWithinBounds =
         x >= 1 && y >= 1 && x + 1 < grid.[y].Length && y + 1 < grid.Length
 
     if isWithinBounds then
-        let word1 = getWord grid 3 (x - 1, y - 1) (1, 1) |> Seq.toArray |> System.String
-        let word2 = getWord grid 3 (x + 1, y + 1) (-1, -1) |> Seq.toArray |> System.String
+        let word1 = getWord grid 3 (x - 1, y - 1) (1, 1)
+        let word2 = getWord grid 3 (x + 1, y - 1) (-1, 1)
 
         if (word1 = "MAS" || word1 = "SAM") && (word2 = "MAS" || word2 = "SAM") then
             1
@@ -40,20 +38,22 @@ let hasDiagonalMas (grid: char[][]) (x: int, y: int) : int =
 
 let countXmasWords (grid: char[][]) (x: int) (y: int) : int =
     direction
-    |> Array.map (fun direction -> getWord grid 4 (x, y) direction |> Seq.toArray |> System.String)
-    |> Array.map (fun word -> if word = targetWord then 1 else 0)
+    |> Array.map (fun direction ->
+        let word = getWord grid 4 (x, y) direction
+
+        if word = "XMAS" then 1 else 0)
     |> Array.sum
 
 let resolvePart1 (lines: seq<string>) : int =
-    let grid = linesToArray lines
+    let grid = linesToGrid lines
 
     grid
     |> Array.mapi (fun y row -> row |> Array.mapi (fun x _ -> countXmasWords grid x y) |> Array.sum)
     |> Array.sum
 
 let resolvePart2 (lines: seq<string>) : int =
-    let grid = linesToArray lines
+    let grid = linesToGrid lines
 
     grid
-    |> Array.mapi (fun y row -> row |> Array.mapi (fun x _ -> hasDiagonalMas grid (x, y)) |> Array.sum)
+    |> Array.mapi (fun y row -> row |> Array.mapi (fun x _ -> hasDiagonalMas grid x y) |> Array.sum)
     |> Array.sum
