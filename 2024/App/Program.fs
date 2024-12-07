bd71dd49f4f9f373fb868e6fe615db335856b75f
Daniel Chromik <daniel.chromik@dawiso.com>
2024-12-07 22:10:08 +0100

    Day 7 part 1 resolved

--- 2024/App/Program.fs
+++ 2024/App/Program.fs
@@ -2,18 +2,17 @@
 
 open System.IO
 
-let days =
-    [| (Day1.resolvePart1, Day1.resolvePart2)
-       (Day2.resolvePart1, Day2.resolvePart2)
-       (Day3.resolvePart1, Day3.resolvePart2)
-       (Day4.resolvePart1, Day4.resolvePart2)
-       (Day5.resolvePart1, Day5.resolvePart2)
-       (Day6.resolvePart1, Day6.resolvePart2)
-       (Day6.resolvePart1, Day6.resolvePart2)
-       (Day7.resolvePart1, Day7.resolvePart2)
-       (Day8.resolvePart1, Day8.resolvePart2)
-       (Day9.resolvePart1, Day9.resolvePart2)
-       (Day10.resolvePart1, Day10.resolvePart2) |]
+// let days =
+//     [| (Day1.resolvePart1, Day1.resolvePart2)
+//        (Day2.resolvePart1, Day2.resolvePart2)
+//        (Day3.resolvePart1, Day3.resolvePart2)
+//        (Day4.resolvePart1, Day4.resolvePart2)
+//        (Day5.resolvePart1, Day5.resolvePart2)
+//        (Day6.resolvePart1, Day6.resolvePart2)
+//        (Day6.resolvePart1, Day6.resolvePart2)
+//        (Day8.resolvePart1, Day8.resolvePart2)
+//        (Day9.resolvePart1, Day9.resolvePart2)
+//        (Day10.resolvePart1, Day10.resolvePart2) |]
 
 [<EntryPoint>]
 let main args =
@@ -24,10 +23,10 @@ let main args =
 
     printfn $"Resolving Day {day}"
 
-    let (resolvePart1, resolvePart2) = days.[(int day) - 1]
+    // let (resolvePart1, resolvePart2) = days.[(int day) - 1]
 
-    let part1 = File.ReadLines $"./day{day}.txt" |> resolvePart1
-    let part2 = File.ReadLines $"./day{day}.txt" |> resolvePart2
+    let part1 = File.ReadLines $"./day{day}.txt" |> Day7.resolvePart1
+    let part2 = File.ReadLines $"./day{day}.txt" |> Day7.resolvePart2
 
     printfn $"Part 1: {part1}"
     printfn $"Part 2: {part2}"
--- 2024/Solutions/Day7.fs
+++ 2024/Solutions/Day7.fs
@@ -1,5 +1,37 @@
 module Day7
 
-let resolvePart1 (lines: seq<string>) : int = 0
+let parseLine (lines: seq<string>) : seq<int64 * seq<int64>> =
+    lines
+    |> Seq.map (fun line ->
+        let splitLine = line.Split(":")
+        let target = splitLine |> Seq.head |> int64
+
+        let numbers =
+            splitLine
+            |> Seq.last
+            |> (fun numbers -> numbers.Trim().Split(" ") |> Array.map int64)
+
+        (target, numbers))
+
+let rec getPermutations (numbers: seq<int64>) (acc: int64) : seq<int64> =
+    seq {
+        match (Seq.tryHead numbers) with
+        | Some(x) ->
+            yield! getPermutations (Seq.tail numbers) (acc + x)
+            yield! getPermutations (Seq.tail numbers) (acc * x)
+        | None -> yield acc
+    }
+
+let resolvePart1 (lines: seq<string>) : int64 =
+    let parsedLines = parseLine lines
+
+    parsedLines
+    |> Seq.map (fun (targetValue, numbers) ->
+        let hasCombination =
+            getPermutations (Seq.tail numbers) (Seq.head numbers)
+            |> Seq.exists (fun res -> res = targetValue)
+
+        if hasCombination then targetValue else 0)
+    |> Seq.sum
 
 let resolvePart2 (lines: seq<string>) : int = 0
