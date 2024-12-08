4f8c649ac739e103aa9fbc0cef471bb4b6f1c1de
Daniel Chromik <daniel.chromik@dawiso.com>
2024-12-08 16:26:20 +0100

    Day8 part 1 WIP

--- 2024/Solutions.Tests/Day8.fs
+++ 2024/Solutions.Tests/Day8.fs
@@ -3,45 +3,29 @@ module Day8
 open NUnit.Framework
 
 let input =
-    @"47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47"
+    @"............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"
 
 [<Test>]
 let Test1 () =
     let arr = input.Split('\n')
 
-    let result = Day5.resolvePart1 arr
-    Assert.AreEqual(143, result)
+    let result = Day8.resolvePart1 arr
+    Assert.AreEqual(14, result)
 
 [<Test>]
 let Test2 () =
     let arr = input.Split('\n')
 
-    let result = Day5.resolvePart2 arr
-    Assert.AreEqual(123, result)
+    let result = Day8.resolvePart2 arr
+    Assert.AreEqual(0, result)
--- 2024/Solutions/Day8.fs
+++ 2024/Solutions/Day8.fs
@@ -1,5 +1,54 @@
 module Day8
 
-let resolvePart1 (lines: seq<string>) : int = 0
+type Grid =
+    { cells: char[][]
+      width: int
+      height: int }
+
+let parseToGrid (lines: seq<string>) : Grid =
+    let cells = lines |> Seq.map (fun line -> line.Trim().ToCharArray()) |> Array.ofSeq
+
+    { cells = cells
+      height = cells.Length
+      width = cells[0].Length }
+
+let getAntennaeGroups (cells: char[][]) =
+    cells
+    |> Seq.mapi (fun y row -> row |> Seq.mapi (fun x cell -> (cell, (x, y))))
+    |> Seq.reduce Seq.append
+    |> Seq.groupBy fst
+    |> Seq.filter (fun (c, _) -> not (c = '.'))
+    |> Seq.map (fun (t, points) -> (t, points |> Seq.map snd |> List.ofSeq))
+
+let distance (a: int * int) (b: int * int) = (fst a - fst b, snd a - snd b)
+
+let addPoints (a: int * int) (b: int * int) = (fst a + fst b, snd a + snd b)
+
+let getAntinodes (a: int * int) (b: int * int) =
+    seq {
+        yield distance a b |> addPoints a
+        yield distance b a |> addPoints b
+    }
+
+let rec findAllAntinodes (a: int * int) (arr: list<int * int>) =
+    seq {
+        match arr with
+        | [] -> ()
+        | b :: tail ->
+            yield! getAntinodes a b
+            yield! findAllAntinodes a tail
+            yield! findAllAntinodes b tail
+    }
+
+let resolvePart1 (lines: seq<string>) : int =
+    let grid = parseToGrid lines
+
+    let antennae =
+        getAntennaeGroups grid.cells
+        |> Seq.map (fun (_, points) -> findAllAntinodes (List.head points) (List.tail points))
+
+    printfn "%A" antennae
+
+    0
 
 let resolvePart2 (lines: seq<string>) : int = 0
