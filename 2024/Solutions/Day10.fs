a0a9d0343909d50f926a38262333edf5b95026c7
Daniel Chromik <daniel.chromik@dawiso.com>
2024-12-10 21:38:12 +0100

    Day 10 solutions added

--- 2024/Solutions/Day10.fs
+++ 2024/Solutions/Day10.fs
@@ -1,5 +1,77 @@
 module Day10
 
-let resolvePart1 (lines: seq<string>) : int = 0
+let parseLineToGrid (lines: seq<string>) =
+    lines
+    |> Seq.map (fun l -> l.Trim().ToCharArray() |> Array.map (fun c -> c.ToString() |> int))
+    |> Seq.toArray
 
-let resolvePart2 (lines: seq<string>) : int = 0
+let getItem (grid: int[][]) (x, y) =
+    grid
+    |> Array.tryItem y
+    |> Option.map (fun row -> row |> Array.tryItem x)
+    |> Option.flatten
+    |> Option.map (fun height -> (height, (x, y)))
+
+let getNeighbours (grid: int[][]) (x, y) =
+    seq {
+        getItem grid (x + 1, y)
+        getItem grid (x - 1, y)
+        getItem grid (x, y + 1)
+        getItem grid (x, y - 1)
+    }
+
+let rec getTrailScore (grid: int[][]) (position: int * int) (height: int) : seq<int * int> =
+    seq {
+        if height = 9 then
+            yield position
+        else
+            let neighbours =
+                getNeighbours grid position
+                |> Seq.choose (fun neighbour ->
+                    match neighbour with
+                    | Some(nextHeight, nextPosition) when nextHeight = (height + 1) -> Some((nextHeight, nextPosition))
+                    | _ -> None)
+
+            for (nextHeight, nextPosition) in neighbours do
+                yield! getTrailScore grid nextPosition nextHeight
+    }
+
+let resolvePart1 (lines: seq<string>) : int =
+    let grid = parseLineToGrid lines
+
+    grid
+    |> Seq.indexed
+    |> Seq.fold
+        (fun totalScore (y, row) ->
+            row
+            |> Seq.indexed
+            |> Seq.fold
+                (fun rowScore (x, trailhead) ->
+                    match trailhead with
+                    | 0 ->
+                        let score = getTrailScore grid (x, y) 0 |> Set.ofSeq |> Seq.length
+
+                        rowScore + score
+                    | _ -> rowScore)
+                totalScore)
+        0
+
+let resolvePart2 (lines: seq<string>) : int =
+    let grid = parseLineToGrid lines
+
+    grid
+    |> Seq.indexed
+    |> Seq.fold
+        (fun totalScore (y, row) ->
+            row
+            |> Seq.indexed
+            |> Seq.fold
+                (fun rowScore (x, trailhead) ->
+                    match trailhead with
+                    | 0 ->
+                        let score = getTrailScore grid (x, y) 0 |> Seq.length
+
+                        rowScore + score
+                    | _ -> rowScore)
+                totalScore)
+        0
