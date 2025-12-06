83c9f88b2ff203f08cdf908a9e06ce000ed4976d
Daniel Chromik <daniel.chromik@proton.me>
2025-12-06 19:56:17 +0100

    Console app logic updated

--- 2025/lib/aoc2025.rb
+++ 2025/lib/aoc2025.rb
@@ -1,3 +1,16 @@
 # frozen_string_literal: true
 
 require_relative 'day1'
+require_relative 'day2'
+
+# Aoc 2025 solutions
+module Aoc2025
+  @solutions = [[Day1.method(:part1), Day1.method(:part2)],
+                [Day2.method(:part1), Day2.method(:part2)]]
+
+  def self.solve(day, part, test)
+    file_path = "inputs/day#{day}#{test ? 'small' : ''}.txt"
+
+    @solutions[day - 1][part - 1].call(file_path)
+  end
+end
---
+++ 2025/lib/day2.rb
@@ -0,0 +1,10 @@
+# Day 2 solutions
+module Day2
+  def self.part1(path)
+    0
+  end
+
+  def self.part2(path)
+    0
+  end
+end
