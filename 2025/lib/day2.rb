e1e4559be8b0ac2da8a5045944eb1676bc23c0de
Daniel Chromik <daniel.chromik@proton.me>
2025-12-07 21:48:24 +0100

    Day 2 solution added

--- 2025/lib/day2.rb
+++ 2025/lib/day2.rb
@@ -1,10 +1,38 @@
 # Day 2 solutions
 module Day2
   def self.part1(path)
-    0
+    result = 0
+    File.foreach(path) do |line|
+      parts = line.split(',')
+
+      parts.each do |part|
+        from, to = part.scan(/\d+|\d+/)
+
+        next if from.nil? || to.nil?
+
+        (from..to).each do |i|
+          result += i.to_i if i.match?(/^(\d+)\1$/)
+        end
+      end
+    end
+    result
   end
 
   def self.part2(path)
-    0
+    result = 0
+    File.foreach(path) do |line|
+      parts = line.split(',')
+
+      parts.each do |part|
+        from, to = part.scan(/\d+|\d+/)
+
+        next if from.nil? || to.nil?
+
+        (from..to).each do |i|
+          result += i.to_i if i.match?(/^(\d+)\1+$/)
+        end
+      end
+    end
+    result
   end
 end
